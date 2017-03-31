a97ab4a9646fdf347a4b23bc1b8f221145ac3ec6
James Fallon <jamesfal27@hotmail.com>
2017-03-31 20:23:28 +0100

    Added scrollable table for outcomes. Added a nicer css

--- printresults.py
+++ printresults.py
@@ -1,14 +1,19 @@
 import plotly.plotly as pltly
 import plotly.graph_objs as go
 import webbrowser
+import operator
 
 
 
-def main (candidateData, chancesOfWinning, mostCommonOutcome):
+def main (candidateData, chancesOfWinning, outcomes, numberOfSeats):
 
     #Create bar chart to display each candidate's chance of winning a seat
 
     candidates = [row[2] + ' ' + row[1] for row in candidateData]
+    candidateFirstName = [row[2] for row in candidateData]
+    candidateSurname = [row[1] for row in candidateData]
+
+    sorted_numberOfWinsPerCandidate = sorted(zip(candidates,chancesOfWinning), key=operator.itemgetter(1))
 
     layout = go.Layout(
         autosize=False,
@@ -23,10 +28,13 @@ def main (candidateData, chancesOfWinning, mostCommonOutcome):
         )
     )
 
+    sortedChanceOfWinning = map(list, zip(*sorted_numberOfWinsPerCandidate))
+
     data = [go.Bar(
-                x=chancesOfWinning,
-                y=candidates,
-                orientation = 'h'
+                x=sortedChanceOfWinning[1],
+                y=sortedChanceOfWinning[0],
+                orientation = 'h',
+                marker=dict(color = '#1ABC9C')
     )]
 
     fig = go.Figure(data=data, layout=layout)
@@ -35,29 +43,33 @@ def main (candidateData, chancesOfWinning, mostCommonOutcome):
 
     #Store the html used to display this chart
 
-    chanceOfWinningChartHtml = '''<h2>Chance of winning a seat: </h2>
+    chanceOfWinningChartHtml = '''<h2>Chance of Winning a Seat: </h2>
                                     <iframe width="1000" height="500" frameborder="0" seamless="seamless" scrolling="no" \
                                     src="''' + plotUrl + '''.embed?width=800&height=550"></iframe>
                                     '''
 
     #Create a html table with the most common outcome
 
-    mostCommonOutcomeTableHtml = '''<h2>Most common outcome:</h2>
-    <table>
+    mostCommonOutcomeTableHtml = '''
+    <h2>Most Common Outcomes: </h2>
+    <div class="scrollingTable">
+    <table id="myTable">
       <tr>
-        <th>Candidate</th>
-        <th>Party</th>
+        <th>Number of<br>Occurences</th>
+        <th colspan="'''+str(numberOfSeats)+'''">Outcome</th>
       </tr>
     '''
 
-    for element in mostCommonOutcome:
-        mostCommonOutcomeTableHtml = mostCommonOutcomeTableHtml + '''<tr>
-          <td>''' + candidates[int(element)] +'''</td>
-          <td><img src="assets/party-logos/''' + candidateData[int(element)][3] + '''.png" alt="" style="width:125px; height:50px;"></td>
-        </tr>
-        '''
-
-    mostCommonOutcomeTableHtml = mostCommonOutcomeTableHtml + '</table>'
+    for i in range(len(outcomes)):
+        outcome,frequency = outcomes[i]
+        outcome = list(outcome)
+        mostCommonOutcomeTableHtml = mostCommonOutcomeTableHtml + '<tr><td align="center" width="1%">' + str(frequency) + '</td>'
+        for element in outcome:
+            mostCommonOutcomeTableHtml = mostCommonOutcomeTableHtml + '''
+              <td align="left"><img src="assets/party-logos/''' + candidateData[int(element)][3] + '''.png" alt="" style="width:50px; height:25px;">&emsp;''' + candidateFirstName[int(element)] + '''&nbsp;''' + candidateSurname[int(element)] + '''</td>
+            '''
+        mostCommonOutcomeTableHtml = mostCommonOutcomeTableHtml + '</tr>'
+    mostCommonOutcomeTableHtml = mostCommonOutcomeTableHtml + '</table></div>'
 
     #Now we add these results to a basic html format and print this to a file
 
@@ -74,20 +86,56 @@ def main (candidateData, chancesOfWinning, mostCommonOutcome):
                 table {
                     font-family: arial, sans-serif;
                     border-collapse: collapse;
+                    border:2px solid #1ABC9C;
                     width: 100%;
                 }
-                td, th {
+                td + td{
+                    font-size: 10px;
                     border: 1px solid #dddddd;
-                    text-align: left;
+                    padding: 8px;
+                }
+                td{
+                    font-size: 16px;
+                    border-left: 2px solid #1ABC9C;
+                    border-right: 2px solid #1ABC9C;
+                    padding: 8px;
+                }
+                th {
+                    border: 1px solid #1ABC9C;
+                    background:#1ABC9C;
+                    text-align: center;
+                    color:white;
+                    font-size: 16px;
                     padding: 8px;
                 }
                 tr:nth-child(even) {
                     background-color: #dddddd;
                 }
+                .scrollingTable {
+                    width: 100%;
+                    overflow-y: auto;
+                }
             </style>
+            <script type="text/javascript">
+            function makeTableScroll() {
+                // Constant retrieved from server-side via JSP
+                var maxRows = 6;
+
+                var table = document.getElementById('myTable');
+                var wrapper = table.parentNode;
+                var rowsInTable = table.rows.length;
+                var height = 0;
+                if (rowsInTable > maxRows) {
+                    for (var i = 0; i < maxRows; i++) {
+                        height += table.rows[i].clientHeight;
+                    }
+                    wrapper.style.height = height + "px";
+                }
+        }
+    </script>
         </head>
-        <body>
-            <h1>Galway-West Local Election</h1>
+        <body onload="makeTableScroll();">
+            <h1>Monte Carlo Simulation Results: Galway-West</h1>
 
             ''' + mostCommonOutcomeTableHtml + chanceOfWinningChartHtml + '''
         </body>
